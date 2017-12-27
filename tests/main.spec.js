81f6ec9e7ce7996537c0c39fcd0642ded7706e32
Mateus Rodrigues <devrdgs@gmail.com>
2017-12-27 11:21:34 -0200

    Created first couple of tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,18 +5,28 @@ const dirtyChai = require('dirty-chai');
 
 const { expect } = chai;
 const { exec } = require('child_process');
+const pkg = require('../package.json');
 
 const btcConverter = './src/main.js';
 
 chai.use(dirtyChai);
 
 describe('Main CLI', () => {
-  it('should return Hello World!', (done) => {
-    exec(btcConverter, (err, stddout, stderr) => {
+  it('should return the current program version', (done) => {
+    exec(`${btcConverter} --version `, (err, stddout) => {
       if (err) {
         throw err;
       }
-      expect(stddout.replace('\n', '')).to.be.equal('Hello World!');
+      expect(stddout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+  it('should return the description when btn-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout) => {
+      if (err) {
+        throw err;
+      }
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true();
       done();
     });
   });
